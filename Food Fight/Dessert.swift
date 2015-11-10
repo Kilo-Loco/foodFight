724bcc7529ddb3b06f13b44c27b85d04f8913917
Kyle Lee <kiloloco@me.com>
2015-11-10 00:01:30 -0800

    Added base class files

---
+++ Food Fight/Dessert.swift
@@ -0,0 +1,13 @@
+//
+//  Dessert.swift
+//  Food Fight
+//
+//  Created by Kyle Lee on 11/9/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+
+import Foundation
+
+class Dessert: Player {
+    
+}
---
+++ Food Fight/Grain.swift
@@ -0,0 +1,13 @@
+//
+//  Grain.swift
+//  Food Fight
+//
+//  Created by Kyle Lee on 11/9/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+
+import Foundation
+
+class Grain: Enemy {
+    
+}
